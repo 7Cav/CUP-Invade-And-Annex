3f982371570da342a10f646a35b59a54fcbcf28b
Micah Whitsel <micah.whitsel@gmail.com>
2021-04-04 07:26:04 -0800

    Arsenal functions library

---
+++ CommonBase/MissionScripts/CfgFunctions.hpp
@@ -0,0 +1,15 @@
+class scripts {
+    class arsenal {
+        file = "MissionScripts\arsenal";
+        class arsenalGeneral {};
+        class arsenalInfantry {};
+        class arsenalLongshot {};
+        class arsenalMedic {};
+        class arsenalLogistics {};
+        class arsenalTanker {};
+        class arsenalHeliPilot {};
+        class arsenalHeliCrew {};
+        class arsenalPlanePilot {};
+        class arsenalAdmin {};
+    }
+}
