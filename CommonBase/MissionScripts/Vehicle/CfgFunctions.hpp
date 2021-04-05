bd2b12ec103cc4df9d1fb83cc7af194be2d50c62
Micah Whitsel <micah.whitsel@gmail.com>
2021-04-05 02:28:30 -0800

    Add vehicle init function library

---
+++ CommonBase/MissionScripts/Vehicle/CfgFunctions.hpp
@@ -0,0 +1,13 @@
+class vehicle
+{
+	class vehicle
+	{
+		file = "MissionScripts\vehicle";
+		class respawnVehicleInitialize {};
+		class respawnVehiclePreInit { preInit = 1; };
+		class respawnVehicleReturn {};
+		class respawnVehicleWhenAbandoned {};
+		class respawnVehicleWhenKilled {};
+		class setVehicleVarName {};
+	};
+};
